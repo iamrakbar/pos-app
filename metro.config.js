1db2d317d6809a3b5b3526b792ead629be57671b
R. Akbar <iamrakbar@gmail.com>
2026-07-27 17:43:28 +0700

    Fixed the fast-scroll disappearing rows in the vendored picker.

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,23 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
+const path = require("node:path");
 
 const config = getDefaultConfig(__dirname);
+const heroUINativeProEntry = path.resolve(
+  __dirname,
+  "vendor/heroui-native-pro/lib/module/index.js"
+);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === "heroui-native-pro") {
+    return {
+      type: "sourceFile",
+      filePath: heroUINativeProEntry,
+    };
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = withUniwindConfig(config, {
   cssEntryFile: "./src/global.css",
--- vendor/heroui-native-pro/lib/module/components/wheel-picker/wheel-picker.js
+++ vendor/heroui-native-pro/lib/module/components/wheel-picker/wheel-picker.js
@@ -5,6 +5,7 @@ import { AnimationSettingsProvider } from 'heroui-native/contexts';
 import { useThemeColor } from 'heroui-native/hooks';
 import { forwardRef, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Platform, Pressable, StyleSheet, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Animated, { useDerivedValue } from 'react-native-reanimated';
 import { HeroText } from "../../helpers/internal/components/index.js";
 import { useAugmentedRef, useControllableState } from "../../helpers/internal/hooks/index.js";
@@ -24,6 +25,7 @@ import { clampIndex, getIndexForValue, getScrollOffsetForIndex, resolveMaskHeigh
  */
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 /**
  * Animated `HeroText` used as the row label for {@link WheelPicker.ItemLabel}.
@@ -448,7 +450,7 @@ function WheelPickerRootInner(props, ref) {
         },
         pointerEvents: isDisabled || isProgrammaticScrolling ? 'none' : 'auto',
         ...restProps,
-        children: [/*#__PURE__*/_jsx(Animated.FlatList, {
+        children: [/*#__PURE__*/_jsx(AnimatedFlatList, {
           ref: listRef,
           data: items,
           renderItem: renderItem,
@@ -459,9 +461,10 @@ function WheelPickerRootInner(props, ref) {
           disableIntervalMomentum: Platform.OS === 'ios' ? true : false,
           showsVerticalScrollIndicator: false,
           scrollEnabled: !isDisabled,
-          initialNumToRender: resolvedVisibleCount + 2,
-          maxToRenderPerBatch: resolvedVisibleCount,
-          windowSize: 3,
+          initialNumToRender: resolvedVisibleCount * 3,
+          maxToRenderPerBatch: resolvedVisibleCount * 3,
+          updateCellsBatchingPeriod: 16,
+          windowSize: 7,
           removeClippedSubviews: false,
           contentContainerStyle: [{
             paddingVertical: verticalPadding
